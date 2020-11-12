ca1a1ccb33322c76653192160e8acddd596c4bd3
JeffShell <875899952@qq.com>
2020-11-12 18:58:35 +0800

    更改æruoter以及添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  链接被狗吃了
+  <hr>
+  <a href="#/">返回首页</a>
+  |
+  <router-link to="/">返回首页2</router-link>
+</div>
+</template>
+
+<script>
+export default {
+name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
