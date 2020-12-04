96fae7c723320554570bc091c981045cc2ada0da
JeffShell <875899952@qq.com>
2020-12-04 12:44:35 +0800

    deploy

---
+++ precache-manifest.3304f27afadfeaa1569ddac6a7e3e649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255d39b16ca86efb232b",
+    "url": "/mccounting-items/css/app.17f110c4.css"
+  },
+  {
+    "revision": "15c2e1437e09c3b9ff007266d5f34978",
+    "url": "/mccounting-items/index.html"
+  },
+  {
+    "revision": "255d39b16ca86efb232b",
+    "url": "/mccounting-items/js/app.993dfaf7.js"
+  },
+  {
+    "revision": "f8531c0199d8b3c15a51",
+    "url": "/mccounting-items/js/chunk-vendors.5180712d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/mccounting-items/manifest.json"
+  },
+  {
+    "revision": "0c6a8ffefe6b4c47848ab37f53dec6bb",
+    "url": "/mccounting-items/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mccounting-items/robots.txt"
+  }
+]);
